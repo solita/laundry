ece1ccece780252edf30d6fac1fe1cbe3de31af0
Erno Kuusela <erno-laptop!erno>
2017-05-26 12:49:11 +0300

    build ghostscript with clang, doesn't compile with gcc

--- build-scripts/ghostscript.bash
+++ build-scripts/ghostscript.bash
@@ -9,15 +9,18 @@ cd "$buildroot"
 
 tarball_url=https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs921/ghostscript-9.21.tar.gz
 
+
 tarball_basename="${tarball_url##*/}"
 mkdir -p downloads
 cd downloads
 test -f $tarball_basename || wget $tarball_url
 cd -
+export CC=clang
+export CXX=clang++
 export CFLAGS="-fsanitize=address -fsanitize=undefined -g -O2 -fPIE -fstack-protector-strong -Wformat -Werror=format-security"
 export CPPFLAGS="-Wdate-time -D_FORTIFY_SOURCE=2"
 export CXXFLAGS="$CFLAGS"
-export "LDFLAGS= -Wl,-Bsymbolic-functions -fPIE -pie -Wl,-z,relro -Wl,-z,now"
+export "LDFLAGS= -Wl,-Bsymbolic-functions -fPIE -pie -Wl,-z,relro -Wl,-z,now -fsanitize=address"
 ncpus=`getconf _NPROCESSORS_ONLN || echo 1`
 mkdir -p src
 tar -C src -zxf "downloads/$tarball_basename"

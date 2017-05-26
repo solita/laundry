ebb06053cbf035ce7403ff4f7762358c9d232c5d
Erno Kuusela <erno-laptop!erno>
2017-05-26 13:50:42 +0300

    Remove ubsan from Ghostscript build - it's nowhere near ubsan clean. Also disable unneeded features at configure time.

--- build-scripts/ghostscript.bash
+++ build-scripts/ghostscript.bash
@@ -17,14 +17,14 @@ test -f $tarball_basename || wget $tarball_url
 cd -
 export CC=clang
 export CXX=clang++
-export CFLAGS="-fsanitize=address -fsanitize=undefined -g -O2 -fPIE -fstack-protector-strong -Wformat -Werror=format-security"
+export CFLAGS="-fsanitize=address -g -O2 -fPIE -fstack-protector-strong -Wformat -Werror=format-security"
 export CPPFLAGS="-Wdate-time -D_FORTIFY_SOURCE=2"
 export CXXFLAGS="$CFLAGS"
-export "LDFLAGS= -Wl,-Bsymbolic-functions -fPIE -pie -Wl,-z,relro -Wl,-z,now -fsanitize=address"
+export "LDFLAGS=-Wl,-Bsymbolic-functions -fPIE -pie -Wl,-z,relro -Wl,-z,now -fsanitize=address"
 ncpus=`getconf _NPROCESSORS_ONLN || echo 1`
 mkdir -p src
 tar -C src -zxf "downloads/$tarball_basename"
 cd src/ghostscript-*
 make clean || true
-./configure --prefix=/opt/laundry > configure.out 2>&1
+./configure --prefix=/opt/laundry --without-x --with-divers=FILES --disable-cups --disable-gtk --disable-dbus > configure.out 2>&1
 make -j $ncpus

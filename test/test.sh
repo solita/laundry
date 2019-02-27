a43ac909f3cbe40aab56006906acd798a9ddf414
Erno Kuusela <erno-laptop!erno>
2019-02-27 17:12:12 +0200

    script syntax fix

--- test/test.sh
+++ test/test.sh
@@ -119,7 +119,7 @@ parse_args() {
 main() {
    parse_args "$@" || return 1
    
-   test -d "$TOOLS/bin" || die "no tools at $TOOLS: `ls -ld $TOOLS/bin 2>&1"
+   test -d "$TOOLS/bin" || die "no tools at $TOOLS: `ls -ld $TOOLS/bin 2>&1`"
 
    # Laundry startup and initial state logging
    start_laundry

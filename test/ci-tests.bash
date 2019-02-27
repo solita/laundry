af961ea49fe8b8c426ce876a5e244db27ed630cb
Erno Kuusela <erno-laptop!erno>
2019-02-27 16:50:45 +0200

    script part of travis config has outgrown the yml, extract to test/ci-tests.bash

---
+++ test/ci-tests.bash
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -u -e -x
+
+# clojure build (to check but also because we run the uberjar later in the e2e test.sh)
+lein uberjar
+
+if [ ! -f $CI_CACHE/laundrybuild.tar ]; then
+    pushd docker-build
+      docker build -t laundrybuild .
+    popd
+    docker save laundrybuild > $CI_CACHE/laundrybuild.tar
+fi
+docker load < $CI_CACHE/laundrybuild.tar
+
+docker run --cap-add SYS_PTRACE -it -e LAUNDRY_BUILD_BRANCH=$TRAVIS_BRANCH laundrybuild
+
+pushd docker-build
+  bash export-opt.bash laundrybuild laundry-opt.tgz
+  docker build -f Dockerfile.programs-runtime -t laundry-programs  .
+popd
+
+# xxx move test.sh to a later stage once it's debugged
+tar -C /opt -xzf docker-build/laundry-opt.tgz && test/test.sh
+
+env LAUNDRY_DOCKER_RUNTIME=runc programs/pdf2pdfa test/testcases/hypno.pdf /tmp/hypno-pdfa.pdf && file /tmp/hypno-pdfa.pdf | grep "PDF document"
+
+pushd programs
+  make docker-libre-image
+popd
+
+docker run -i -e docconv_suffix=doc --rm --name convrun1 libreconv < test/testcases/test.doc > /tmp/docresult.tar
+tar --to-stdout -xf /tmp/docresult.tar out/document.pdf | file - | grep "PDF document"
+docker run -i -e docconv_suffix=docx --rm --name convrun1 libreconv < test/testcases/test.docx > /tmp/docxresult.tar
+tar --to-stdout -xf /tmp/docxresult.tar out/document.pdf | file - | grep "PDF document"
+
+# - ldd /opt/laundry/bin/gs
+# - /opt/laundry/bin/pdf2txt test/testcases/hypno.pdf /tmp/out.txt && cat /tmp/out.txt

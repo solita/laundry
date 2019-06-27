870f7785bf65e4f36507e9cbd01527fa73a1b899
Erno Kuusela <erno-laptop!erno>
2019-06-27 16:37:33 +0300

    replace opensagres lib based docx conversion (take 2)

--- src/laundry/docx.clj
+++ src/laundry/docx.clj
@@ -11,40 +11,12 @@
              [clojure.set :as set]
              [clojure.java.io :as io]
              [clojure.pprint :refer [pprint]])
-  (:import (java.io ByteArrayOutputStream ByteArrayInputStream StringWriter File)
-           (org.apache.poi.xwpf.usermodel XWPFDocument)
-           (fr.opensagres.poi.xwpf.converter.pdf PdfConverter PdfOptions)))
+  (:import (java.io ByteArrayOutputStream ByteArrayInputStream StringWriter File)))
 
 
-(defn- filename-as-pdf-suffix [filename]
-  (string/replace-first filename #"(?i)\.(\w*)" ".pdf"))
-
-(defn docx->pdf [filename-info input-juttu]
-  ;; input-juttu = lähde, voi olla input-streamille kelpaava olio eli InputStream, File, URI, URL,Socket, byte array, tai String
-  ;; filename-infolla ei tehdä muuta kuin vaihdetaan tiedostopääte
-  
-  (with-open [in (io/input-stream input-juttu)]
-    (let [doc (XWPFDocument. in)
-          first-bytesink (ByteArrayOutputStream.)]
-
-      ;; Täytyy olla tyylit, vaikkakin vain tyhjät.  https://stackoverflow.com/questions/51330192/trying-to-make-simple-pdf-document-with-apache-poi      
-      (.createStyles doc)
-      ;; "document must be written so underlaaying objects will be committed" -> ylimääräisen XWPFDocument:n luonti.
-      (.write doc first-bytesink)
-      (.close doc)
-
-      ;; Avataan ekan writen tulos taas input streamiksi
-      (with-open [in (io/input-stream (.toByteArray first-bytesink))]
-        (let [new-doc (XWPFDocument. in)
-              final-bytesink (ByteArrayOutputStream.)]
-          (-> (PdfConverter/getInstance) (.convert new-doc final-bytesink (PdfOptions/create)))
-          (.close new-doc)
-          {:name  (filename-as-pdf-suffix filename-info)
-           :mime  "application/pdf"
-           :bytes (.toByteArray final-bytesink)})))))
 
 (s/defn api-docx2pdf [env, tempfile :- java.io.File]
-  (let [res (docx->pdf "file.docx" (io/input-stream tempfile))]
+  (let [res (shell-out! (str (:tools env) "/bin/docx2pdf") path out)]
       (.delete tempfile) ;; todo: move to finally block
       (if (:bytes res)
         (htresp/content-type 
@@ -61,6 +33,6 @@
             :middleware [upload/wrap-multipart-params]
             (let [tempfile (:tempfile file)
                   filename (:filename file)]
-               (info "PDF converter received " filename "(" (:size file) "b)")
+               (info "docx converter received " filename "(" (:size file) "b)")
                (.deleteOnExit tempfile) ;; cleanup if VM is terminated
                (api-docx2pdf env tempfile))))))

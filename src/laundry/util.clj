557768bf0e24f87a4ba79a5e1af2c64b3a4eedd0
Erno Kuusela <erno-laptop!erno>
2019-07-05 16:16:40 +0300

    logging tweak

--- src/laundry/util.clj
+++ src/laundry/util.clj
@@ -4,10 +4,10 @@
    [clojure.java.shell :as shell]))
 
 (defn shell-out! [binary-path input-path output-path]
-  (try
-    (info "exception resistant sh invocation attmept!" )
+  (try    
     (shell/sh binary-path input-path output-path)
     (catch java.io.IOException ex ;; sometimes throws instead of returning error, contrary to docs
+      (warn "java.shell/sh threw exception, translating as exit value 10000" )
       {:out "" :err "" :exit 10000 :exception ex})))
 
 

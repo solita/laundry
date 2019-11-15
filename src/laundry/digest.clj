6176f08e01af9deec5ce8f721c371930e5d8c081
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2019-11-15 11:46:56 +0200

    Cleanup: reformat digest.clj
    Sort deps, reindent for consistency and remove trailing whitespace.

--- src/laundry/digest.clj
+++ src/laundry/digest.clj
@@ -1,38 +1,39 @@
 (ns laundry.digest
-  (:require [compojure.api.sweet :as sweet :refer :all]
-             [ring.util.http-response :refer [ok status content-type] :as resp]
-             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-             [ring.swagger.upload :as upload]
-             [taoensso.timbre :as timbre :refer [trace debug info warn]]
-             [schema.core :as s]
-             [clojure.java.shell :refer [sh]]
-             [laundry.machines :as machines]
-             [clojure.java.io :as io]))
+  (:require
+   [clojure.java.io :as io]
+   [clojure.java.shell :refer [sh]]
+   [compojure.api.sweet :as sweet :refer :all]
+   [laundry.machines :as machines]
+   [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+   [ring.swagger.upload :as upload]
+   [ring.util.http-response :refer [ok status content-type] :as resp]
+   [schema.core :as s]
+   [taoensso.timbre :as timbre :refer [trace debug info warn]]))
 
 ;(machines/add-command-line-rule!
 ;    [nil "--checksum-command COMMAND" "compute a checksum"
 ;       :default "/opt/laundry/bin/checksum"
 ;       :id :checksum-command])
-    
+
 (s/defschema DigestAlgorithm
-   (s/enum "sha256"))
+  (s/enum "sha256"))
 
 (s/defn api-checksum [env, tempfile :- java.io.File, digest :- DigestAlgorithm]
-   (let [res (sh (str (:tools env) "/bin/checksum") (.getAbsolutePath tempfile) digest)]
-      (.delete tempfile)
-      (if (= (:exit res) 0)
-         (ok (:out res))
-         (machines/badness-resp "digest computation failed"))))
+  (let [res (sh (str (:tools env) "/bin/checksum") (.getAbsolutePath tempfile) digest)]
+    (.delete tempfile)
+    (if (= (:exit res) 0)
+      (ok (:out res))
+      (machines/badness-resp "digest computation failed"))))
 
-(machines/add-api-generator! 
-   (fn [env] 
-      (sweet/context "/digest" []
-         (POST "/sha256" []
-            :summary "compute a SHA256 digest for posted data"
-            :multipart-params [file :- upload/TempFileUpload]
-            :middleware [upload/wrap-multipart-params]
-            (let [tempfile (:tempfile file)
-                  filename (:filename file)]
-               (info "SHA256 received " filename "(" (:size file) "b)")
-               (.deleteOnExit tempfile)
-               (api-checksum env tempfile "sha256"))))))
+(machines/add-api-generator!
+ (fn [env]
+   (sweet/context "/digest" []
+     (POST "/sha256" []
+       :summary "compute a SHA256 digest for posted data"
+       :multipart-params [file :- upload/TempFileUpload]
+       :middleware [upload/wrap-multipart-params]
+       (let [tempfile (:tempfile file)
+             filename (:filename file)]
+         (info "SHA256 received " filename "(" (:size file) "b)")
+         (.deleteOnExit tempfile)
+         (api-checksum env tempfile "sha256"))))))

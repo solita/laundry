3769a4fb3c2add91b3124d55b9e9f68713c39aef
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2019-11-15 11:00:30 +0200

    Add clojure test for /digest/sha256

--- project.clj
+++ project.clj
@@ -16,7 +16,8 @@
    :target-path "target/%s/"
    :profiles
       {:dev
-       {:dependencies [[ring-mock "0.1.5"]]
+       {:dependencies [[peridot "0.5.2"]
+                       [ring-mock "0.1.5"]]
           :resource-paths ["target/generated"]
           :plugins [[lein-cljsbuild "1.1.7"]]}
        :uberjar

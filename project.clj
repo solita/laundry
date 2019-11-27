23600f7d17366092ea8f825c65b04ab63c0a83ec
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2019-11-27 13:00:02 +0200

    Reformat project.clj (sort dependencies, remove trailing whitespace)

--- project.clj
+++ project.clj
@@ -1,35 +1,32 @@
 (defproject laundry "0.0.0-SNAPSHOT"
-   :dependencies 
-      [[org.clojure/clojure "1.9.0"]
-       [prismatic/schema "1.1.9"]
-       [ring/ring "1.7.0"]
-       [ring/ring-defaults "0.3.2"]
-       [ring-basic-authentication "1.0.5"]
-       [metosin/compojure-api "1.1.11"]
-       [clj-http "3.9.1"]
-       [hiccup "1.0.5"]
-       [enlive "1.1.6"]
-       [org.clojure/tools.cli "0.4.1"]
+   :dependencies
+      [[clj-http "3.9.1"]
        [com.fzakaria/slf4j-timbre "0.3.12"]
-       [com.taoensso/timbre "4.10.0"]]
-   
+       [com.taoensso/timbre "4.10.0"]
+       [enlive "1.1.6"]
+       [hiccup "1.0.5"]
+       [metosin/compojure-api "1.1.11"]
+       [org.clojure/clojure "1.9.0"]
+       [org.clojure/tools.cli "0.4.1"]
+       [prismatic/schema "1.1.9"]
+       [ring-basic-authentication "1.0.5"]
+       [ring/ring "1.7.0"]
+       [ring/ring-defaults "0.3.2"]]
    :source-paths ["src"]
    :target-path "target/%s/"
-   :profiles 
-      {:dev 
+   :profiles
+      {:dev
        {:dependencies [[ring-mock "0.1.5"]]
           :resource-paths ["target/generated"]
           :plugins [[lein-cljsbuild "1.1.7"]]}
-       :uberjar 
+       :uberjar
           {:main  laundry.main
            :aot   [laundry.main]
            :uberjar-name "laundry.jar"}}
-           
-   :repl-options 
+   :repl-options
       {:init-ns laundry.server
        :init (println "Now (go)")
        :timeout 900000 ; 90s, needed for slow machines
        }
-       
    :main laundry.main
 )

efc8f26ef98d9c233eb63eaccd2b8a284404598e
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2019-11-28 08:51:38 +0200

    Cleanup: reformat schemas.clj

--- src/laundry/schemas.clj
+++ src/laundry/schemas.clj
@@ -1,14 +1,15 @@
 (ns laundry.schemas
-   (:require [schema.core :as s]))
+  (:require
+   [schema.core :as s]))
 
-(s/defschema Status 
-   (s/enum "ok" "error"))
+(s/defschema Status
+  (s/enum "ok" "error"))
 
 (s/defschema LaundryConfig
-   {:port s/Num
-    :slow-request-warning s/Num
-    :temp-directory s/Str
-    :tools s/Str
-    :basic-auth-password (s/maybe (s/pred fn?))
-    :log-level (s/enum :debug :info)})
+  {:port s/Num
+   :slow-request-warning s/Num
+   :temp-directory s/Str
+   :tools s/Str
+   :basic-auth-password (s/maybe (s/pred fn?))
+   :log-level (s/enum :debug :info)})
 

95485d6a8edc6a04a2a51114da0442ca7a45ad42
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2019-11-28 10:08:36 +0200

    Cleanup: reformat util.clj

--- src/laundry/util.clj
+++ src/laundry/util.clj
@@ -1,13 +1,11 @@
 (ns laundry.util
   (:require
-   [taoensso.timbre :as timbre :refer [trace debug info warn]]
-   [clojure.java.shell :as shell]))
+   [clojure.java.shell :as shell]
+   [taoensso.timbre :as timbre :refer [trace debug info warn]]))
 
 (defn shell-out! [binary-path input-path output-path]
-  (try    
+  (try
     (shell/sh binary-path input-path output-path)
     (catch java.io.IOException ex ;; sometimes throws instead of returning error, contrary to docs
-      (warn ex "java.shell/sh threw exception, translating as exit value 10000" )
+      (warn ex "java.shell/sh threw exception, translating as exit value 10000")
       {:out "" :err "" :exit 10000 :exception ex})))
-
-

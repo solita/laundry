af3ac8e99e156d2a1394e6f57a226baaa059eaca
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2019-12-13 09:37:05 +0200

    Add test for /image/jpeg2jpeg

---
+++ test/laundry/image_test.clj
@@ -0,0 +1,27 @@
+(ns laundry.image-test
+  (:require
+   [clojure.java.io :as io]
+   [clojure.test :refer :all]
+   [laundry.test.fixture :as fixture]
+   [peridot.multipart]
+   [ring.mock.request :as mock])
+  (:import
+   (javax.imageio ImageIO)))
+
+(deftest ^:integration api-image-jpeg2jpeg
+  (let [app (fixture/get-app)
+        file (io/file (io/resource "test.jpg"))
+        _ (assert file)
+        orig-img (ImageIO/read file)
+        request (-> (mock/request :post "/image/jpeg2jpeg")
+                    (merge (peridot.multipart/build {:file file})))
+        response (app request)
+        content-type (get-in response [:headers "Content-Type"])]
+    (is (= 200 (:status response)))
+    (is (= "image/jpeg" content-type))
+    (when (= "image/jpeg" content-type)
+      (let [img (ImageIO/read (:body response))]
+        (is (some? img)
+            "Response is an image")
+        (is (= (.getWidth orig-img) (.getWidth img)))
+        (is (= (.getHeight orig-img) (.getHeight img)))))))

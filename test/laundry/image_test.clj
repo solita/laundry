41a3ae3080e35eb27a951018ecfe7616d7e8fe64
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2019-12-13 09:37:30 +0200

    Add test for /image/png2png

--- test/laundry/image_test.clj
+++ test/laundry/image_test.clj
@@ -25,3 +25,21 @@
             "Response is an image")
         (is (= (.getWidth orig-img) (.getWidth img)))
         (is (= (.getHeight orig-img) (.getHeight img)))))))
+
+(deftest ^:integration api-image-png2png
+  (let [app (fixture/get-app)
+        file (io/file (io/resource "test.png"))
+        _ (assert file)
+        orig-img (ImageIO/read file)
+        request (-> (mock/request :post "/image/png2png")
+                    (merge (peridot.multipart/build {:file file})))
+        response (app request)
+        content-type (get-in response [:headers "Content-Type"])]
+    (is (= 200 (:status response)))
+    (is (= "image/png" content-type))
+    (when (= "image/png" content-type)
+      (let [img (ImageIO/read (:body response))]
+        (is (some? img)
+            "Response is an image")
+        (is (= (.getWidth orig-img) (.getWidth img)))
+        (is (= (.getHeight orig-img) (.getHeight img)))))))

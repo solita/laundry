95753c7d5a7179f01d6bab65ab31e283c0785158
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2019-12-19 09:18:54 +0200

    Add test for /pdf/pdf-preview

--- test/laundry/pdf_test.clj
+++ test/laundry/pdf_test.clj
@@ -5,7 +5,27 @@
    [laundry.test.fixture :as fixture]
    [peridot.multipart]
    [ring.mock.request :as mock]
-   [ring.util.request]))
+   [ring.util.request])
+  (:import
+   (javax.imageio ImageIO)))
+
+(deftest ^:integration api-pdf-pdf-preview
+  (let [app (fixture/get-app)
+        file (io/file (io/resource "testcases/hypno.pdf"))
+        _ (assert file)
+        request (-> (mock/request :post "/pdf/pdf-preview")
+                    (merge (peridot.multipart/build {:file file})))
+        response (app request)
+        content-type (get-in response [:headers "Content-Type"])]
+    (is (= 200 (:status response)))
+    (is (= "image/jpeg" content-type))
+    (when (= "image/jpeg" content-type)
+      (let [img (ImageIO/read (:body response))]
+        (is (some? img)
+            "Response is an image")
+        (when img
+          (is (= 612 (.getWidth img)))
+          (is (= 792 (.getHeight img))))))))
 
 (deftest ^:integration api-pdf-pdf2pdfa
   (let [app (fixture/get-app)

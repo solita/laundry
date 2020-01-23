41b298f058ebdf963c7159bd08a32a081dfb5855
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2020-01-23 11:09:47 +0200

    Add compojure.route/not-found to routes

--- src/laundry/server.clj
+++ src/laundry/server.clj
@@ -6,6 +6,7 @@
    [clojure.set :as set]
    [clojure.string :as string]
    [compojure.api.sweet :as sweet :refer :all]
+   [compojure.route]
    [laundry.config :as config]
    [laundry.docx :as docx]
    [laundry.image :as image]
@@ -64,7 +65,10 @@
          :middleware [(create-auth-middleware env)]
          (GET "/auth-test" []
            (ok))
-         (apply routes api-calls))))
+         (apply routes api-calls))
+
+       (undocumented
+         (compojure.route/not-found (resp/not-found)))))
 
 (defn laundry-path-stripper [handler]
   ;; accommodate getting served requests with /laundry prefix

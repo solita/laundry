cecbf10b811409ff33be5638ab6de720f936c22b
timoma <timo.mantyvaara@solita.fi>
2021-03-19 11:23:57 +0200

    Use port arg and correct Dockerfile

--- docker-build/build-and-run.sh
+++ docker-build/build-and-run.sh
@@ -18,8 +18,8 @@ cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 32 >> api-key
 echo "Using api key: $(cat api-key)"
 
 echo Building laundry
-docker build -t laundry --build-arg PORT=$port --file Dockerfile.laundry ./
+docker build -t laundry --build-arg PORT=$port --file Dockerfile2 ./
 rm api-key
 
 echo Run laundry
-docker run -d --rm -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock --group-add "$(cut -d: -f3 < <(getent group docker))" -e LAUNDRY_DOCKER_RUNTIME=runc laundry
+docker run -d --rm -p $port:$port -v /var/run/docker.sock:/var/run/docker.sock --group-add "$(cut -d: -f3 < <(getent group docker))" -e LAUNDRY_DOCKER_RUNTIME=runc laundry

f471231ca571d599f41d20c7773bc01c3e313aff
Erno Kuusela <erno!erno-laptop>
2021-08-16 14:34:14 +0300

    terraform template from docs

---
+++ gcp/terraform/main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  project = "hanki-2361-laundry-dev"
+  region  = "europe-north1"
+  zone    = "europe-north1-a"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}

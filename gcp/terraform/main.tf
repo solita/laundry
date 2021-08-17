38573ee4f60e879d5272647ffd03ae1bbca8db50
Vladislav Melnitsuk <vladislav.melnitsuk@solita.fi>
2021-08-17 13:23:37 +0300

    compute instance template

--- gcp/terraform/main.tf
+++ gcp/terraform/main.tf
@@ -4,21 +4,14 @@ provider "google" {
   zone    = "europe-north1-a"
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
+resource "google_compute_instance_from_template" "tpl" {
+  name = "instance-from-template"
+  source_instance_template = "laundry-appserver-2021-01-15v1"
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  network_interface {
-    # A default network is created for all GCP projects
-    network = "default"
-    access_config {
-    }
+  // Override fields from instance template
+  can_ip_forward = false
+  labels = {
+    my_key = "my_value"
   }
 }
 
@@ -26,3 +19,5 @@ resource "google_compute_network" "vpc_network" {
   name                    = "terraform-network"
   auto_create_subnetworks = "true"
 }
+
+

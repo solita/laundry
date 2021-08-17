2bca3cea89d4475369a5676cdd194343b3aef239
Vladislav Melnitsuk <vladislav.melnitsuk@solita.fi>
2021-08-17 14:42:53 +0300

    Instance_group & load_balancer

--- gcp/terraform/main.tf
+++ gcp/terraform/main.tf
@@ -11,6 +11,66 @@ provider "google" {
   zone    = "europe-north1-a"
 }
 
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
+
+resource "google_compute_region_backend_service" "default" {
+  load_balancing_scheme = "INTERNAL_MANAGED"
+
+  backend {
+    group          = google_compute_region_instance_group_manager.rigm.instance_group
+    balancing_mode = "UTILIZATION"
+    capacity_scaler = 1.0
+  }
+
+  name        = "laundry-dev-service"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  health_checks = [google_compute_region_health_check.default.id]
+}
+
+resource "google_compute_region_health_check" "default" {
+  name   = "rbs-health-check"
+  http_health_check {
+    port_specification = "USE_SERVING_PORT"
+  }
+}
+
+resource "google_compute_region_instance_group_manager" "rigm" {
+  name     = "rbs-rigm"
+  version {
+    instance_template = google_compute_instance_template.instance_template.id
+    name              = "primary"
+  }
+  base_instance_name = "internal-glb"
+  target_size        = 1
+}
+
+data "google_compute_image" "debian_image" {
+  family   = "debian-9"
+  project  = "debian-cloud"
+}
+
+resource "google_compute_instance_template" "instance_template" {
+  name         = "template-region-service"
+  machine_type = "e2-medium"
+
+    network_interface {
+    network    = "default"
+  }
+
+  disk {
+    source_image = data.google_compute_image.debian_image.self_link
+    auto_delete  = true
+    boot         = true
+  }
+
+  tags = ["allow-ssh", "load-balanced-backend"]
+}
+
 resource "google_compute_instance_from_template" "tpl" {
   name = "instance-from-template"
   source_instance_template = "laundry-appserver-2021-01-15v1"
@@ -20,9 +80,4 @@ resource "google_compute_instance_from_template" "tpl" {
   labels = {
     my_key = "my_value"
   }
-}
-
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
 }

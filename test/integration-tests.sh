7978bcb958d8ba3787875376500d88c7f15394d1
Erno Kuusela <erno!erno-laptop>
2022-10-04 08:26:57 +0300

    ansible python version vs docker module fix

--- test/integration-tests.sh
+++ test/integration-tests.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eux
 
-ansible-galaxy install --role-file=ansible/dependencies.yml --roles-path=ansible/roles
-ansible-playbook --connection=local -i localhost, ansible/build.yml ansible/playbook.yml
+ansible-galaxy install --role-file=ansible/dependencies.yml --roles-path=ansible/roles -e ansible_python_interpreter=python3
+ansible-playbook --connection=local -i localhost, ansible/build.yml ansible/playbook.yml  -e ansible_python_interpreter=python3
 
 ln -s programs bin
 env LAUNDRY_DOCKER_RUNTIME=runc lein test :integration
